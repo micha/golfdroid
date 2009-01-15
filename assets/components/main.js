3f9654ba6264e6a52539e12fe916c1137802eb5f
Micha Niskin <asdf@ubergibson.com>
2009-01-15 17:45:39 -0500

    update of example app

--- assets/components/main.js
+++ assets/components/main.js
@@ -1,3 +1,16 @@
 $(".top_image").append("<img src='file:///android_asset/images/main_banner_watermark.png'/>");
 
+var doit = function() {
+	var canvas = $(".mycanvas").get()[0];
+	var ctx = canvas.getContext("2d");
+	
+	ctx.fillStyle = "rgb(200,0,0)";
+	ctx.fillRect(10,10,55,50);
+	
+	ctx.fillStyle = "rgba(0,0,200,0.5)";
+	ctx.fillRect(30,30,55,50);
+}
+
+doit();
+
 window.java.notify(1, "golfdroid test", "page loaded!");
